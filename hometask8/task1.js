8428614d0ccc967d9cfabc423df59f2c29e12087
Elena <108807767+elenaberdnik@users.noreply.github.com>
2025-10-03 12:46:45 +0300

    hometask7 (#10)

---
+++ hometask8/task1.js
@@ -0,0 +1,11 @@
+const student = {}; 
+
+    student.firstName = "Olena";
+    student.lastName = "Berdnyk";
+    student.age = 31;
+    student.courses = [
+        "Math", "History", "Programming"];
+    
+
+console.log(student);
+
