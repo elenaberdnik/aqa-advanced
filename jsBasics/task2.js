653d0d5fcad68e18f669395fe4d1ea15ffbd483b
Elena <108807767+elenaberdnik@users.noreply.github.com>
2025-10-14 15:44:57 +0300

    hometask11

--- jsBasics/task2.js
+++ jsBasics/task2.js
@@ -3,3 +3,4 @@ const name2 = "Ivanna";
 const greeting =  "Hello " + name1 + " and "+ name2 + "!";
 console.log (greeting);
 const greeting2 = `Hello ${name1} and ${name2}`
+console.log (greeting2);
